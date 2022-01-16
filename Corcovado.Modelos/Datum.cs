23447c803cca8fdc4845dc415a2c68ec3123bba7
lucascampos@id.uff.br <lucascampos@id.uff.br>
2022-01-16 11:31:34 -0300

    Calculo Datum Termino da classe e calculos

--- Corcovado.Modelos/Datum.cs
+++ Corcovado.Modelos/Datum.cs
@@ -11,6 +11,16 @@ namespace Corcovado.Modelos
         public double _a { get; set; }
         public double _b { get; set; }
 
+
+        public double lat_a { get; set; }
+        public double lat_b { get; set; }
+        public double lon_a { get; set; }
+        public double lon_b { get; set; }
+
+
+        public DateTime data_a { get; set; }
+        public DateTime data_b { get; set; }
+
         public int SINAL(double num)
         {
             if (num == 0)
@@ -73,10 +83,7 @@ namespace Corcovado.Modelos
             return Math.PI;
         }
 
-        public double lat_a { get; set; }
-        public double lat_b { get; set; }
-        public double lon_a { get; set; }
-        public double lon_b { get; set; }
+        
 
         #region FM
         public double fm_decimal { get; set; }
@@ -225,5 +232,262 @@ namespace Corcovado.Modelos
         }
         #endregion
 
+        #region D
+        public double D { get; set; }
+        public void CalculoD()
+        {
+            D = ((3 * ((Math.Pow(_a, 2) - Math.Pow(_b, 2)) / Math.Pow(_a, 2))) * SEN(RADIANOS(lat_a)) * COS(RADIANOS(lat_a)) * 4.84813681107636E-06) / (2 * Math.Pow(((1 - ((Math.Pow(_a, 2) - Math.Pow(_b, 2)) / Math.Pow(_a, 2)) * Math.Pow(SEN(RADIANOS(lat_a)), 2))) ,(3 / 2)));
+        }
+        #endregion
+
+        #region E
+        public double E { get; set; }
+        public void CalculoE()
+        {
+            E = (1 + 3 * Math.Pow(TAN(RADIANOS(lat_a)) , 2)) / (6 * Math.Pow(NA , 2));
+        }
+        #endregion
+
+        #region K1
+        public double K1 { get; set; }
+        public void CalculoK1()
+        {
+            K1 = B * Y1;
+        }
+        #endregion
+
+        #region K2
+        public double K2 { get; set; }
+        public void CalculoK2()
+        {
+            K2 = C * Math.Pow(X1 , 2);
+        }
+        #endregion
+
+        #region K3
+        public double K3 { get; set; }
+        public void CalculoK3()
+        {
+            K3 = D * Math.Pow(K1, 2);
+        }
+        #endregion
+
+        #region K4
+        public double K4 { get; set; }
+        public void CalculoK4()
+        {
+            K4 = E * K1 * Math.Pow(X1, 2);
+        }
+        #endregion
+
+
+        #region K5
+        public double K5 { get; set; }
+        public void CalculoK5()
+        {
+            K5 = df_segundo - K2 -K3 + Math.Pow(K4, 2);
+        }
+        #endregion
+
+
+        #region Y
+        public double Y { get; set; }
+        public void CalculoY()
+        {
+            Y =K5 / B;
+        }
+        #endregion
+
+
+        #region C1
+        public double C1 { get; set; }
+        public void CalculoC1()
+        {
+            C1 = (Math.Pow((C / B) , 2)) * (Math.Pow(X1 , 3)) * (2 / 3);
+        }
+        #endregion
+
+        #region C2
+        public double C2 { get; set; }
+        public void CalculoC2()
+        {
+            C2 = 0.0000000000000004095 * X1 * Math.Pow(Y1 ,2);
+        }
+        #endregion
+
+        #region A1B
+        public double A1B { get; set; }
+        public void CalculoA1B()
+        {
+            A1B = 1 / (NB * 4.84813681107636E-06 * COS(RADIANOS(lat_b)));
+        }
+        #endregion
+
+        #region W
+        public double W { get; set; }
+        public void CalculoW()
+        {
+            W = dl_segundo / A1B;
+        }
+        #endregion
+
+        #region X
+        public double X { get; set; }
+        public void CalculoX()
+        {
+            X = W - C1 - C2;
+        }
+        #endregion
+
+        #region distancia_m
+        public double distancia_m { get; set; }
+        public void CalculoDistanciaM()
+        {
+            distancia_m = RAIZ(Math.Pow(X , 2) + Math.Pow(Y ,2));
+        }
+        #endregion
+
+        #region contra_azimute
+        public double contra_azimute_decimal { get; set; }
+        public double contra_azimute_grau { get; set; }
+        public double contra_azimute_minuto { get; set; }
+        public double contra_azimute_segundo { get; set; }
+
+        public void CalculoContraAzimuteDecimal()
+        {
+            if (X < 0)
+            {
+                contra_azimute_decimal = 360 - (GRAUS(ACOS(Y / (RAIZ(Math.Pow(Y , 2) + Math.Pow(X , 2))))));
+            }
+            else
+            {
+                contra_azimute_decimal = (GRAUS(ACOS(Y / (RAIZ(Math.Pow(Y, 2) + Math.Pow(X, 2))))));
+            }
+        }
+
+        public void CalculoContraAzimuteGrau()
+        {
+            contra_azimute_grau = (INT(contra_azimute_decimal * SINAL(contra_azimute_decimal)) * SINAL(contra_azimute_decimal));
+        }
+
+        public void CalculoContraAzimuteMinuto()
+        {
+            contra_azimute_minuto = INT((contra_azimute_decimal * SINAL(contra_azimute_decimal) - (INT(contra_azimute_decimal * SINAL(contra_azimute_decimal)))) * 60);
+        }
+
+        public void CalculoContraAzimuteSegundo()
+        {
+            contra_azimute_segundo = ((((contra_azimute_decimal * SINAL(contra_azimute_decimal) - (INT(contra_azimute_decimal * SINAL(contra_azimute_decimal)))) * 60)) 
+                - INT((((contra_azimute_decimal * SINAL(contra_azimute_decimal) - (INT(contra_azimute_decimal * SINAL(contra_azimute_decimal)))) * 60)))) * 60;
+        }
+
+        #endregion
+
+
+        #region AzimuteB
+        public double azimute_b_decimal { get; set; }
+        public double azimute_b_grau { get; set; }
+        public double azimute_b_minuto { get; set; }
+        public double azimute_b_segundo { get; set; }
+
+        public void CalculoAzimuteBDecimal()
+        {
+            if (contra_azimute_decimal > 180)
+            {
+                azimute_b_decimal = contra_azimute_decimal - 180;
+            }
+            else
+            {
+                azimute_b_decimal = contra_azimute_decimal + 180;
+            }
+        }
+
+        public void CalculoAzimuteBGrau()
+        {
+            azimute_b_grau = (INT(azimute_b_decimal * SINAL(azimute_b_decimal)) * SINAL(azimute_b_decimal));
+        }
+
+        public void CalculoAzimuteBMinuto()
+        {
+            azimute_b_minuto = INT((azimute_b_decimal * SINAL(azimute_b_decimal) - (INT(azimute_b_decimal * SINAL(azimute_b_decimal)))) * 60);
+        }
+
+        public void CalculoAzimuteBSegundo()
+        {
+            azimute_b_segundo = ((((azimute_b_decimal * SINAL(azimute_b_decimal) - (INT(azimute_b_decimal * SINAL(azimute_b_decimal)))) * 60)) 
+                - INT((((azimute_b_decimal * SINAL(azimute_b_decimal) - (INT(azimute_b_decimal * SINAL(azimute_b_decimal)))) * 60)))) * 60;
+        }
+
+        #endregion
+
+        #region rumo_gds
+        public double rumo_gds { get; set; }
+        public void CalculoRumoGDS()
+        {
+            if (azimute_b_decimal >=0 && azimute_b_decimal < 90)
+            {
+                rumo_gds = azimute_b_decimal;
+            }
+            else if (azimute_b_decimal >= 90 && azimute_b_decimal <= 180)
+            {
+                rumo_gds = 180 - azimute_b_decimal;
+            }
+            else if (azimute_b_decimal > 180 && azimute_b_decimal <= 270)
+            {
+                rumo_gds = azimute_b_decimal - 180;
+            }
+            else
+            {
+                rumo_gds = 360 - azimute_b_decimal;
+            }
+        }
+        #endregion
+
+        #region quadrante
+        public string quadrante { get; set; }
+        public void CalculoQuadrante()
+        {
+            if (azimute_b_decimal >= 0 && azimute_b_decimal < 90)
+            {
+                quadrante = "NE";
+            }
+            else if (azimute_b_decimal >= 90 && azimute_b_decimal <= 180)
+            {
+                quadrante = "SE";
+            }
+            else if (azimute_b_decimal > 180 && azimute_b_decimal <= 270)
+            {
+                quadrante = "SW";
+            }
+            else
+            {
+                quadrante = "NO";
+            }
+        }
+        #endregion
+
+        #region dif_hora
+        public double dif_hora { get; set; }
+        public void CalculoDiffHora()
+        {
+            dif_hora = data_b.Subtract(data_a).TotalHours;
+        }
+        #endregion
+
+        #region velocidade_km
+        public double velocidade_km { get; set; }
+        public void CalculoVelocidadeKm()
+        {
+            velocidade_km = (distancia_m / 1000.0) / dif_hora;
+        }
+        #endregion
+
+        #region velocidade_knots
+        public double velocidade_knots { get; set; }
+        public void CalculoVelocidadeKnots()
+        {
+            velocidade_knots = velocidade_km * 0.539957;
+        }
+        #endregion
     }
 }
